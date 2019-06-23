6592e9137fac8a4079951fc156a4fb045372e28a
Anri Liy <anri@realquantum.com>
2019-06-23 16:20:00 -0500

    Add router

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Do you have any questions?</h2>
+                <p>Please call to this number. we are only serving from phone</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+    render() {
+        return(
+            <div>
+                <h2>STUFF</h2>
+                <p>This is Our team</p>
+                <ol>
+                    <li>Taiji</li>
+                    <li>Sayaka</li>
+                    <li>Chi~</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
